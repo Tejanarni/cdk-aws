b0756143041b6f53902f41e49f30caab297aa0a4
Teja Narni <75619472+Tejanarni@users.noreply.github.com>
2023-05-03 01:33:48 +0530

    test

--- apikey.py
+++ apikey.py
@@ -1,28 +1,10 @@
-from aws_cdk import (
-    core,
-    aws_apigatewayv2 as apigw,
-)
+public class Main {
+    public static void main(String[] args) {
+        String token = "<your_token_value>";
+        System.setProperty("API_TOKEN", token);
 
-class MyStack(core.Stack):
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
-        super().__init__(scope, id, **kwargs)
+        // Your application code here...
+    }
+}
 
-        # Create the HTTP API
-        api = apigw.HttpApi(self, "MyHttpApi")
 
-        # Create the API key
-        api_key = apigw.CfnApiKey(self, "MyApiKey",
-            api_key_name="MyApiKey",
-            enabled=True,
-            generate_distinct_id=True,
-            value="my-api-key-value"
-        )
-
-        # Attach the API key to the HTTP API
-        apigw.CfnDeployment(self, "MyDeployment",
-            api_id=api.http_api_id,
-            stage_name=api.default_stage.stage_name,
-            stage_variables={
-                "ApiKeyId": api_key.ref
-            }
-        )
