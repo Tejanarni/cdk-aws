947fd4a4a3cf780ca7373444a5c14984cc4c6c71
Teja Narni <75619472+Tejanarni@users.noreply.github.com>
2023-05-31 00:47:03 +0530

    Update schedular.py

--- schedular.py
+++ schedular.py
@@ -23,19 +23,46 @@ def lambda_handler(event, context):
     service_names = body['ecs']['service_names']
     service_desired_count = int(body['ecs']['service_desired_count'])
     
-    if ('start' == action):
-        start_rds_instances(dbclusteridentifier)
-        start_ecs_tasks(cluster, service_names, service_desired_count)
-    
-    elif (action == 'stop'):
-        stop_rds_instances(dbclusteridentifier)
-        stop_ecs_tasks(cluster, service_names, service_desired_count)
+    try:
+        if action == 'start':
+            start_rds_response = start_rds_instances(dbclusteridentifier)
+            start_ecs_response = start_ecs_tasks(cluster, service_names, service_desired_count)
+            
+            response_body = {
+                'statusCode': 200,
+                'body': json.dumps("Script execution completed. See CloudWatch logs for complete output"),
+                'start_rds_response': start_rds_response,
+                'start_ecs_response': start_ecs_response,
+                'errorLogs': get_error_logs()
+            }
+        elif action == 'stop':
+            stop_rds_response = stop_rds_instances(dbclusteridentifier)
+            stop_ecs_response = stop_ecs_tasks(cluster, service_names, service_desired_count)
+            
+            response_body = {
+                'statusCode': 200,
+                'body': json.dumps("Script execution completed. See CloudWatch logs for complete output"),
+                'stop_rds_response': stop_rds_response,
+                'stop_ecs_response': stop_ecs_response,
+                'errorLogs': get_error_logs()
+            }
+        else:
+            response_body = {
+                'statusCode': 400,
+                'body': json.dumps("Invalid action: {0}".format(action))
+            }
+    except Exception as e:
+        logger.error("An error occurred: {0}".format(str(e)))
+        response_body = {
+            'statusCode': 500,
+            'body': json.dumps("An error occurred: {0}".format(str(e))),
+            'errorLogs': get_error_logs()
+        }
+
+    return response_body
 
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps("Script execution completed. See Cloudwatch logs for complete output")
-    }
+
 
    
 ### start rds instances
@@ -117,3 +144,36 @@ def stop_ecs_tasks(cluster, service_names, service_desired_count):
             'statusCode': 500,
             'body': json.dumps("Error updating ECS tasks: " + str(e))
         }
+
+
+### Get CloudWatch logs for the Lambda function
+def get_error_logs():
+    log_group_name = '/aws/lambda/{0}'.format(os.environ['AWS_LAMBDA_FUNCTION_NAME'])
+    logs_client = boto3.client('logs')
+    
+    try:
+        response = logs_client.describe_log_streams(
+            logGroupName=log_group_name,
+            orderBy='LastEventTime',
+            descending=True,
+            limit=1
+        )
+        
+        if 'logStreams' in response and len(response['logStreams']) > 0:
+            log_stream_name = response['logStreams'][0]['logStreamName']
+            
+            response = logs_client.get_log_events(
+                logGroupName=log_group_name,
+                logStreamName=log_stream_name,
+                startFromHead=True
+            )
+            
+            error_logs = []
+            for event in response['events']:
+                error_logs.append(event['message'])
+            
+            return error_logs
+    except Exception as e:
+        logger.error("An error occurred while retrieving CloudWatch logs: {0}".format(str(e)))
+
+    return []
