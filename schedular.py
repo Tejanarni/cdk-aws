5bc229326c7ce5a73593f9dee5db92682c8bf662
Teja Narni <75619472+Tejanarni@users.noreply.github.com>
2023-05-30 23:46:06 +0530

    Update schedular.py

--- schedular.py
+++ schedular.py
@@ -36,3 +36,60 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps("Script execution completed. See Cloudwatch logs for complete output")
     }
+
+   
+### start rds instances
+def start_rds_instances(dbclusteridentifier):
+    try:
+        #rds.start_db_instance(dbclusteridentifierIdentifier=dbclusteridentifier)
+        rds.start_db_cluster(DBClusterIdentifier=dbclusteridentifier)
+        logger.info('Success :: start_db_instance ' + dbclusteridentifier) 
+    except ClientError as e:
+        logger.error(e)   
+    return "started:OK"
+
+### start ecs tasks
+def start_ecs_tasks(cluster, service_names, service_desired_count):
+    for service_name in service_names.split(","):
+        try:
+            response = client.update_service(
+                cluster=cluster,
+                service=service_name,
+                desiredCount=service_desired_count
+            )
+            logger.info("Updated {0} service in {1} cluster with desired count set to {2} tasks".format(service_name, cluster, service_desired_count))
+        except Exception as e:
+            logger.error("Error updating {0} service in {1} cluster: {2}".format(service_name, cluster, e))
+    return {
+        'statusCode': 200,
+        'new_desired_count': service_desired_count
+    }
+
+### stop rds instances
+def stop_rds_instances(dbclusteridentifier):
+    try:
+        #rds.stop_db_instance(dbclusteridentifierIdentifier=dbclusteridentifier)
+        rds.stop_db_cluster(DBClusterIdentifier=dbclusteridentifier)
+        logger.info('Success :: stop_db_instance ' + dbclusteridentifier) 
+
+    except ClientError as e:
+        logger.error(e)   
+    return "stopped:OK"
+
+
+### stop ecs tasks
+def stop_ecs_tasks(cluster, service_names, service_desired_count):
+    for service_name in service_names.split(","):
+        try:
+            response = client.update_service(
+                cluster=cluster,
+                service=service_name,
+                desiredCount=service_desired_count
+            )
+            logger.info("Updated {0} service in {1} cluster with desired count set to {2} tasks".format(service_name, cluster, service_desired_count))
+        except Exception as e:
+            logger.error("Error updating {0} service in {1} cluster: {2}".format(service_name, cluster, e))
+    return {
+        'statusCode': 200,
+        'new_desired_count': service_desired_count
+    }
